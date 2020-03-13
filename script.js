c654b58a0bd5e89d5bdcaecf3e07bf5acb449ff3
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-03-12 22:48:55 -0700

    added in alert password

--- script.js
+++ script.js
@@ -3,48 +3,37 @@ var generateBtn = document.querySelector("#generate");
 // let upperCaseChars = all the uppercase characters
 // let numberChars = all the number characters
 // let special = all the allowed special characters
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var special = ["!", "#", "$", "%", "&", "'", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCasechar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialchar = ["!", "#", "$", "%", "&", "'", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+var upperCasechar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberchar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 // When the user clicks generate
 generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  // prompt user for number of chars
-  var passwordLength = prompt("How many characters do you want the password to contain?");
-  // save result in variable called passwordLength
-  if (7 < passwordLength && passwordLength <129) {
-  var lowerCase = confirm("did you want to use Lowercase?");
-  var upperCase = confirm("did you want to use Uppercase?");
-  var special = confirm("did you want to use Special characters?");
-  var number = confirm("did you want to use Numbers?");
-  }
-  else {
-    alert("Password must be within 8 to 128 characters");
-  }
-};
 
-  //else if (passwordLength < 129) {
-  //var lowerCase = confirm("did you want to use Lowercase?");
-  //var upperCase = confirm("did you want to use Uppercase?");
-  //var special = confirm("did you want to use Special characters?");
-  //var number = confirm("did you want to use Numbers?");
-  
-//psuedo startup help
-// var passwordText = document.querySelector("#password");
-// passwordText.value = password;
-// var password = generatePassword();
-// let lowerCaseChars = all the lowercase characters
-// let upperCaseChars = all the uppercase characters
-// let numberChars = all the number characters
-// let special = all the allowed special characters
-// When the user clicks generate
-// prompt user for number of chars
-// save result in variable called passwordLength
-// IF passwordLength is greater than 7 and less than 129
-//   assign each of the following results to variables
-//   let useNums = confirm user want to use numbers
-//   let useLcase = confirm user wants lowercase characters
-//   let useUcase = confirm user wants uppercase
-//   let useSpecial = confirm user wants special chars
+  // prompt user for number of chars
+  // save result in variable called passwordLength
+  var passwordLength = prompt("How many characters do you want the password to contain?");
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  //conditions for password generator 
+  if (7 < passwordLength && passwordLength < 129) {
+    var lowerCase = confirm("did you want to use Lowercase?");
+    var upperCase = confirm("did you want to use Uppercase?");
+    var specialkeys = confirm("did you want to use Special characters?");
+    var number = confirm("did you want to use Numbers?");
+    
+}
+else{
+      alert("Password must be within 8 to 128 characters");}
+
+      generatePassword();
+    
+
+function generatePassword() {
+
+      return password;
+}}
